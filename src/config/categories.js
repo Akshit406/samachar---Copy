a6099e59a409cb257a652da6fd417b3f19eb32f7
Akshit406 <akshitsaxena450@gmail.com>
2025-08-02 11:47:44 +0530

    added categories for news

---
+++ src/config/categories.js
@@ -0,0 +1,20 @@
+// src/config/categories.js
+
+export const categories = {
+  health: "Health___Fitness",
+  education: "education",
+  fashion: "fashion",
+  miscellaneous: "miscellaneous",
+  travel: "travel",
+  science: "science",
+  automobile: "automobile",
+  world: "world",
+  entertainment: "entertainment",
+  startup: "startup",
+  technology: "technology",
+  sports: "sports",
+  politics: "politics",
+  business: "business",
+  national: "national",
+  top_stories: "top_stories", // fallback category
+};
