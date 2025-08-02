05003fd648df4151c0dc8dce5933f7c74289890b
Akshit406 <akshitsaxena450@gmail.com>
2025-08-02 12:27:30 +0530

    updated categories

--- src/config/categories.js
+++ src/config/categories.js
@@ -1,20 +1,18 @@
-// src/config/categories.js
-
 export const categories = {
-  health: "Health___Fitness",
-  education: "education",
-  fashion: "fashion",
-  miscellaneous: "miscellaneous",
-  travel: "travel",
-  science: "science",
-  automobile: "automobile",
-  world: "world",
-  entertainment: "entertainment",
-  startup: "startup",
-  technology: "technology",
-  sports: "sports",
-  politics: "politics",
-  business: "business",
-  national: "national",
-  top_stories: "top_stories", // fallback category
+  health: 'health',
+  education: 'education',
+  fashion: 'fashion',
+  miscellaneous: 'miscellaneous',
+  travel: 'travel',
+  science: 'science',
+  automobile: 'automobile',
+  world: 'world',
+  entertainment: 'entertainment',
+  startup: 'startup',
+  technology: 'technology',
+  sports: 'sports',
+  politics: 'politics',
+  business: 'business',
+  national: 'national',
+  top_stories: 'top_stories', // fallback category
 };
