bf2edcce0d8904dff31d1a83e1207ac2af781b20
Akshit406 <akshitsaxena450@gmail.com>
2025-08-02 20:50:07 +0530

    daily ping setup

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (_, res) => res.send('Bot is alive!'));
+
+app.listen(PORT, () => {
+  console.log(`🌐 Keep-alive server running on port ${PORT}`);
+});
